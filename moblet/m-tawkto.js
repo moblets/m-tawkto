14cae68b6d93eced5bfc475b8fcc0fe2d12336bb
Leonardo "Alemax" Fonseca Ferreira <leualemax@gmail.com>
2017-03-06 10:58:05 -0300

    master

--- moblet/m-tawkto.js
+++ moblet/m-tawkto.js
@@ -32,22 +32,50 @@ module.exports = {
     // iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(fakeBody);
 
     // element.append(iframe);
-    window.Tawk_API = window.Tawk_API || {};
-    window.Tawk_API.disableMobileBackHistory = true;
-    var s1 = document.createElement("script");
-    var s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/588b2e1163b0fb7e39a76aaa/default';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+
+
+    scope.div = document.createElement('div');
+
+    scope.div.classList.add("iframe-holder-tawkto");
+    scope.iframe = document.createElement('iframe');
+    scope.div.appendChild(scope.iframe);
+    element[0].appendChild(scope.div);
 
   },
-  controller: function() {
+  controller: function($scope, $timeout, $mAppDef, $mDataLoader) {
+    $scope.moblet.isLoading = true;
+    $scope.$watch("iframe", function() {
+      console.log($mAppDef.info());
+      console.log($scope.moblet);
+
+      var dataLoadOptions = {
+        offset: 0,
+        cache: false
+      };
+      $mDataLoader.load($scope.moblet, dataLoadOptions)
+          .then(function(data) {
+            console.log(data);
+            $scope.iframe.src = "http://m.dev.app.vc/id/777118/tawkto/588b2e1163b0fb7e39a76aaa";
+          }
+        );
+    });
 
 
+    // $scope.moblet.isLoading = true;
 
-    window.Tawk_API.maximize();
+
+    // window.Tawk_API = window.Tawk_API || {};
+    // window.Tawk_API.disableMobileBackHistory = true;
+    // window.Tawk_API.onChatStarted = function() { window.Tawk_API.maximize(); }
+    //  ;
+    // };
+    // var s1 = document.createElement("script");
+    // var s0 = document.getElementsByTagName("script")[0];
+    // s1.async = true;
+    // s1.src = 'https://embed.tawk.to/588b2e1163b0fb7e39a76aaa/default';
+    // s1.charset = 'UTF-8';
+    // s1.setAttribute('crossorigin', '*');
+    // s0.parentNode.insertBefore(s1, s0);
     // function layout() {
     //   $timeout(function() {
     //     document.querySelector("#tawkchat-container").style.top = "43px";
@@ -59,9 +87,7 @@ module.exports = {
     // window.Tawk_API = window.Tawk_API || {};
     // window.Tawk_API.disableMobileBackHistory = true;
 
-    // window.Tawk_API.onChatStarted = function() {
-    //   layout();
-    // };
+
 
     // window.Tawk_API.onLoad = function() {
     //   layout();
